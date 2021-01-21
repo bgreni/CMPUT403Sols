306159364eb970373510042de64b9d9948594ffe
bgreni <grenierb96@gmail.com>
2021-01-20 18:37:31 -0700

    starting open problems

---
+++ Open/foosball.py
@@ -0,0 +1,29 @@
+import sys
+"""
+  Brian Grenier
+  1545276
+  bgrenier
+
+  List any resources you used below (eg. urls, name of the algorithm from our code archive).
+  Remember, you are permitted to get help with general concepts about algorithms
+  and problem solving, but you are not permitted to hunt down solutions to
+  these particular problems!
+
+    N/A
+
+  List any classmate you discussed the problem with. Remember, you can only
+  have high-level verbal discussions. No code should be shared, developed,
+  or even looked at in these chats. No formulas or pseudocode should be
+  written or shared in these chats.
+
+  N/A
+
+  By submitting this code, you are agreeing that you have solved in accordance
+  with 
+"""
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+
+    players = sys.stdin.readline().split()
+    
